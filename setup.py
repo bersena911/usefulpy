30c09beb279cd005a33aa12fc8c74a5e0064f24c
bersena911 <matebersenadze@gmail.com>
2024-02-01 16:01:16 +0400

    raise version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='usefulpy',
     packages=find_packages(),
-    version='0.6',
+    version='0.7',
     license='MIT',
     description='The most useful functions in everyday life',
     long_description=long_description,
